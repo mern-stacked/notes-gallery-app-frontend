69b800f6587a45c6bedaf68909ac765faee9f7dd
eshwagadkar <eshwagadkar007@gmail.com>
2023-06-01 14:36:57 +0530

    App.js modified

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
+import { Ionicons } from '@expo/vector-icons';
 
 import AccountScreen from "./src/screens/AccountScreen";
 import CreateNoteScreen from "./src/screens/CreateNoteScreen";
@@ -9,10 +10,12 @@ import ListNoteScreen from "./src/screens/ListNotesScreen";
 import NotesDetailScreen from "./src/screens/NotesDetailScreen";
 import SigninScreen from "./src/screens/SigninScreen";
 import SignupScreen from "./src/screens/SignupScreen";
+import AdminLoginScreen from "./src/screens/AdminLoginScreen";
 
 import { Provider as AuthProvider } from './src/context/AuthContext';
+import { setNavigator } from "./src/navigationRef";
 
-//
+// Home Screens
 const HomeStack = createNativeStackNavigator();
 
 function HomeStackScreen() {
@@ -26,7 +29,7 @@ function HomeStackScreen() {
   );
 }
 
-
+// Notes List and Detail Screens
 const NotesListStack = createNativeStackNavigator();
 
 function NotesListStackScreen() {
@@ -38,6 +41,7 @@ function NotesListStackScreen() {
   );
 }
 
+// Authentication Screens
 const AuthStack = createNativeStackNavigator();
 
 function AuthStackScreen() {
@@ -53,15 +57,46 @@ function AuthStackScreen() {
   );
 }
 
+// Bottom Tab Navigator for Auhentication
 const Tab = createMaterialBottomTabNavigator();
+
+function TabNavigator() {
+  return (
+    <Tab.Navigator 
+         screenOptions={({ route }) => ({
+         tabBarIcon: ({ focused, color, size }) => {  
+         let iconName;
+         if (route.name === 'Admin Login') {
+            iconName = focused ? 'ios-person' : 'ios-person-outline';
+         } else if (route.name === 'User Login') {
+            iconName = focused ? 'ios-people' : 'ios-people-outline';
+         }
+        
+         return <Ionicons name={iconName} size={22} color={color}     />;
+        },
+        })}
+        tabBarOptions={{
+        activeTintColor: 'red',
+        inactiveTintColor: 'gray',
+        }} >
+      <Tab.Screen name="Admin Login" component={AdminLoginScreen} />
+      <Tab.Screen name="User Login" component={AuthStackScreen} />
+  </Tab.Navigator>
+  );
+}
 
 const Navigation = () => {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="All Notes" component={NotesListStackScreen} />
-        <Tab.Screen name="Authenticate" component={AuthStackScreen} />
-      </Tab.Navigator>
+    <NavigationContainer >  
+      <HomeStackScreen />
+    </NavigationContainer>
+  );
+}
+
+const Navigation1 = () => {
+  return (
+    <NavigationContainer >  
+      <TabNavigator />
     </NavigationContainer>
   );
 }
@@ -69,7 +104,7 @@ const Navigation = () => {
 export default App = () => {
   return (
     <AuthProvider>
-      <Navigation />
+      <Navigation1 />
     </AuthProvider>
   );
 }
