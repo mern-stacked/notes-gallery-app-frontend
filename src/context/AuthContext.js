a8a9af07364ef190bc6d3b970fdb55833fb6084e
eshwagadkar <eshwagadkar007@gmail.com>
2023-06-03 03:48:18 +0530

    Fix the signup feature

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,4 @@
-// import { storage } from "../mmkvStorage";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import createDataContext from "./createDataContext";
 import serverAPI from '../api/server';
 import SigninScreen from '../screens/SigninScreen';
@@ -20,29 +20,18 @@ const authReducer = (state, action ) => {
 
 
 const signup = (dispatch) =>  async ({ uname, email, password, designation }) =>  {
-        // Make a API request to signup with that entered email and password
-        try{
+    try{
+            // Make a API request to signup with that entered email and password
             const response = await serverAPI.post('/users/signup', { name: uname, email, password, designation });
-            console.log(response.data.token)
-          
-            // dispatch({ type: 'signup', payload: response.data.token  });
-
-            // await AsyncStorage.setItem('token', response.data.token );
-            navigate('UserLogin', { uname } )
-
-            // dispatch({ type: 'signup', payload: { message: 'User created successfully. Please Login' , token: response.data.token} });
-            // RootNavigation.navigate('UserLogin', { uname } )
-            navigationRef.current.getRootState()
-
-            
+            // If we signup, modify our state, and say that we are authenticated  
+            await AsyncStorage.setItem('token', response.data.token );
+            dispatch({ type: 'signup', payload: response.data.token  });
+            navigate('UserLogin', { uname } )       
+            // If signing up fails, manage it
         } catch (err) {
             dispatch({ type: 'signup_error', payload: 'Something went wrong with signup.' })
         }
         
-        // If we signup, modify our state, and say that we are authenticated  
-
-
-        // If signing up fails, manage it
     };
 
 
