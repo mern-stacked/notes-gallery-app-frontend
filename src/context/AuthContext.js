733ef1121e8169b882f2468be655a560f4118936
eshwagadkar <eshwagadkar007@gmail.com>
2023-06-03 05:02:44 +0530

    SignIn Feature

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,22 +1,25 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import createDataContext from "./createDataContext";
 import serverAPI from '../api/server';
-import SigninScreen from '../screens/SigninScreen';
-import { navigate, navigationRef } from '../RootNavigation';
-
-// import { navigate } from '../navigationRef';
-import * as RootNavigation from '../RootNavigation';
+import { navigate } from '../RootNavigation';
 
 const authReducer = (state, action ) => {
     switch(action.type){
         case 'signup_error':
             return { ...state, message: action.payload }
-        case 'signup':
+        case 'signin':
             return { message: '',  token: action.payload }
+        case 'clear_error_message':
+            return { ...state, message: '' }
         default:
             return state;
     }
 };  
+
+
+const clearErrorMessage = dispatch => () => {
+    dispatch({ type: 'clear_error_message' });
+}
 
 
 const signup = (dispatch) =>  async ({ uname, email, password, designation }) =>  {
@@ -25,7 +28,7 @@ const signup = (dispatch) =>  async ({ uname, email, password, designation }) =>
             const response = await serverAPI.post('/users/signup', { name: uname, email, password, designation });
             // If we signup, modify our state, and say that we are authenticated  
             await AsyncStorage.setItem('token', response.data.token );
-            dispatch({ type: 'signup', payload: response.data.token  });
+            dispatch({ type: 'signin', payload: response.data.token  });
             navigate('UserLogin', { uname } )       
             // If signing up fails, manage it
         } catch (err) {
@@ -35,19 +38,21 @@ const signup = (dispatch) =>  async ({ uname, email, password, designation }) =>
     };
 
 
-const signin = (dispatch) => {
-    return () => {
+const signin = (dispatch) => async ({ email, password }) => {
+    try{
+        // Make a API request to signup with that entered email and password
+        const response = await serverAPI.post('/users/login', { email, password });
+        // If we signup, modify our state, and say that we are authenticated  
+        await AsyncStorage.setItem('token', response.data.token );
+        dispatch({ type: 'signin', payload: response.data.token  });
+        navigate('Account', { email } )       
+        // If signing up fails, manage it
+    } catch (err) {
+        console.log(err)
+        dispatch({ type: 'signup_error', payload: 'Something went wrong with signin.' })
+    }
+};
 
-        // Try signing in
-
-        // Handle Success by updating state 
-
-        // Handle failure by showing error message
-
-
-
-    };
-}
 
 const signout = (dispatch) => {
     return () => {
@@ -57,6 +62,6 @@ const signout = (dispatch) => {
 
 export const { Provider, Context } = createDataContext(
     authReducer,
-    {signup, signin, signout},
+    {signup, signin, signout, clearErrorMessage},
     { token: null, message: '' }
 );
