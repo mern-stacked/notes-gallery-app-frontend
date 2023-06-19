fa82f1d2528059a0f4134c1177972712448bed30
eshwagadkar <eshwagadkar007@gmail.com>
2023-06-19 10:28:22 +0530

    mofication account screen

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,20 +1,28 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { View, StyleSheet, SafeAreaView } from "react-native";
 import { Text, Input, Button, Avatar } from '@rneui/themed';
 import Spacer from '../components/Spacer';
 import { Context as AuthContext } from '../context/AuthContext';
 
 
-const AccountScreen = ({ navigation }) => {
+const AccountScreen = ({ route, navigation }) => {
     
-    const { state,  signout } = useContext(AuthContext);
+    const { state, signout, fetchUserDetails } = useContext(AuthContext);
+    
+    useEffect(() => {
+        const unsubscribe = navigation.addListener('focus', () => {
+            // console.log('hi there')
+        });
+      return unsubscribe;
+    }, [navigation])
   
     return(
         <SafeAreaView forceInset={{ top: 'always' }}>
             <View style={styles.container}> 
-                <Text h3>Hello, User </Text>
-                <Text></Text>
-
+                <Text h4>Hello, <Text h2>{state.userName}</Text></Text>
+                <Text h6>Your Email id is: <Text h4>{state.userEmail}</Text></Text>
+                <Text h6>Signed in as: <Text h4>{state.userDesignation}</Text></Text>
+                
                 <Spacer>
                     <Button title="Sign Out"
                     onPress={signout} />
