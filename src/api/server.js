6a094563dacde8d49506076da564fee96c55569a
eshwagadkar <eshwagadkar007@gmail.com>
2023-06-22 11:42:14 +0530

    attach token to headers of an api request

--- src/api/server.js
+++ src/api/server.js
@@ -1,7 +1,24 @@
 import axios from "axios";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default axios.create({
+const instance =  axios.create({
     baseURL: 'https://notes-zone-api.onrender.com/api/',
     timeout: 1000,
     headers: {'X-Custom-Header': 'foobar'}
-  });
+});
+
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem('token')
+
+    if(token){
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err)
+  }
+);
+
+export default instance;
